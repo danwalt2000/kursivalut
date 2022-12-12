05b560f5d4b0bb1a218440e6bc5fd807968d35ae
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2022-12-12 17:33:30 +0300

    fonts loaded from my server

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -19,7 +19,6 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
         <link href="/css/app.css" rel="stylesheet">
         <link rel="icon" type="image/x-icon" alt="icon" href="/img/valuta.ico">
         <link rel="canonical" href="https://{{Request::url()}}" />
