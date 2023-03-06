29adb95a49aa2c45eb9a9bbda978be5d60fa2d2f
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2023-03-06 10:05:06 +0300

    search form styled

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -44,6 +44,7 @@
                     <form action="{{ url("s") }}" class="search-form" method="get">
                         <input id="search" type="text" placeholder="Поиск в объявлениях" name="search" value=""
                             class="search-input @error('search') is-invalid @enderror" minlength="1" maxlength="30" required>
+                        <label class="search-label" for="search">Поиск в объявлениях</label>
                         <button id="search-submit" type="submit" class="search-submit" title="Поиск">
                             <img alt="Поиск" class="search" src="/img/search.svg" alt="" width="18" height="18">
                         </button>  
