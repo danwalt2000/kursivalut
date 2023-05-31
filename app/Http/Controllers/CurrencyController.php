e6234431725faf71d9063d4fd8fed6e330646419
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2023-05-31 15:53:42 +0300

    rates view activated

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -56,7 +56,7 @@ class CurrencyController extends Controller
             'date_sort'       => Config::get('common.date_sort'),
             'path'            => $this->path,
             'query'           => $this->query,
-            // 'rates'           => $this->rates->getRatesByLocale( $this->locale ),
+            'rates'           => $this->rates->getRatesByLocale( $this->locale ),
             'title'           => ParseUriController::generateTitle(),
             'hash'            => $this->getCurrentGitCommit(),
             'h1'              => ParseUriController::getH1(),
