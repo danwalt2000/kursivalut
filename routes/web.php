23fa40c0dc09cf85d16c8347d80cf37040abacae
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2023-11-23 19:01:39 +0300

    acme challenge route added

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('/{landing}', [CurrencyController::class, 'landing'])
      ->where(['landing' => 'legal']);
 
 Route::get("sitemap.xml" , [CurrencyController::class, 'sitemap']);
+
+Route::get('/.well-known/acme-challenge/{token}', function (string $token) {
+     return \Illuminate\Support\Facades\Storage::get('public/.well-known/acme-challenge/' . $token);
+ });
