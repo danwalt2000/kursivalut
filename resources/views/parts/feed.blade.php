227a8ae26dedef0b346ee39ecfd58e2fc353fda1
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2024-11-06 12:31:52 +0300

    restricted description changed

--- resources/views/parts/feed.blade.php
+++ resources/views/parts/feed.blade.php
@@ -48,7 +48,7 @@
     <article class="post no-ads">
         <img class="no-ads-img" src="/img/no-ads.svg" title="Объявлений не найдено" alt="Объявлений не найдено" loading="lazy">
         <h2 class="">Доступ к ленте ограничен для вашего региона</h2>
-        <div class="post-content post-content_left">В соответствии с местным законодательством, доступ к объявлениям, на основании которых собирается статистика, ограничен в вашем регионе.</div>
+        <div class="post-content post-content_left">В соответствии с требованием РКН от 01.11.2024 года, доступ к объявлениям, на основании которых собирается статистика, ограничен в вашем регионе.</div>
         <div class="post-content post-content_left mt-10"><span class="hidden_phone spoiler_text">Включите VPN.</span></div>
     </article>
 @endif
