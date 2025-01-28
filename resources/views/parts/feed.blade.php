c37de20db6f1fe84bf935048f2b589017b20dd45
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2025-01-28 15:58:10 +0300

    some layout changes

--- resources/views/parts/feed.blade.php
+++ resources/views/parts/feed.blade.php
@@ -8,7 +8,7 @@
     <article class="post no-ads">
         <img class="no-ads-img" src="/img/no-ads.svg" title="Объявлений не найдено" alt="Объявлений не найдено" loading="lazy">
         <h2 class="">Доступ к ленте ограничен для вашего региона</h2>
-        <a class="no-ads-tg-link" href="https://t.me/kursivalut_ru_{{ $locale['name'] }}" target="_blank">Открыть ленту в Telegram</a>
+        <a class="no-ads-tg-link" href="https://t.me/kursivalut_ru_{{ $locale['name'] }}" target="_blank"><strong>Открыть <span class="no-ads-hidden-mobile">ленту</span> в Telegram</strong></a>
         <div class="post-content post-content_left">В соответствии с требованием РКН от 01.11.2024 года, доступ к объявлениям, на основании которых собирается статистика, ограничен в вашем регионе.</div>
         <div class="post-content post-content_left mt-10"><span class="hidden_phone spoiler_text">Лента доступна с VPN.</span></div>
     </article>
