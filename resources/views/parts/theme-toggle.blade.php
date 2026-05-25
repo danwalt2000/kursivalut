361086edf2653680dbc66c856e7c77a97d57edd6
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2026-05-25 13:24:10 +0300

    new design layot fixes

--- resources/views/parts/theme-toggle.blade.php
+++ resources/views/parts/theme-toggle.blade.php
@@ -1,4 +1,4 @@
 <button type="button" class="theme-toggle" data-theme-toggle aria-pressed="false" aria-label="Переключить тему">
     <svg alt="Светлая тема" class="theme-toggle-light" width="18" height="18"><use href="/img/sun.svg"></use></svg>
-    <svg alt="Темная тема" class="theme-toggle-dark" width="16" height="16"><use href="/img/moon.svg"></use></svg>
+    <svg alt="Темная тема" class="theme-toggle-dark" width="18" height="18"><use href="/img/moon.svg"></use></svg>
 </button>
